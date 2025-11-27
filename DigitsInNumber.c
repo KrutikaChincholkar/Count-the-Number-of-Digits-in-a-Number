5fc3a511f4fd8646377422346cf732d16d0d6f93
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2025-11-27 23:48:52 +0530

    Add digits counting program in DigitsInNumber.c
    Implement a program to count the number of digits in an integer.
    Example:
    Number = 5234
    Steps:
    5234 → 523 → 52 → 5 → 0
    Count = 4 digits

---
+++ DigitsInNumber.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    int num, count = 0, temp;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    temp = num;
+
+    if (temp == 0) {
+        count = 1;
+    } else {
+        while (temp != 0) {
+            temp /= 10;
+            count++;
+        }
+    }
+
+    printf("Number of digits: %d", count);
+
+    return 0;
+}
